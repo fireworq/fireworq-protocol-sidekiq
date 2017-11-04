c0b4961d840884369139d161fad8195edc43cf91
tarao <tarao.gnn@gmail.com>
2017-11-04 23:38:58 +0900

    Options.

--- script/app.rb
+++ script/app.rb
@@ -14,7 +14,14 @@ set :logging, false
 post '/job/:category' do
   request.body.rewind
   job = JSON.parse(request.body.read)
-  Fireworq::Launcher.perform_async(job)
+  run_after = job['run_after'] || 0
+
+  # Ignore handling these options since it is not easy to make them
+  # take effect in Sidekiq jobs:
+  # - retry_delay
+  # - max_retries
+
+  Fireworq::Launcher.perform_in(run_after, job)
   job.to_json
 end
 
@@ -24,6 +31,7 @@ end
 module Fireworq
   class Launcher
     include Sidekiq::Worker
+    sidekiq_options queue: ENV['QUEUE_NAME']
 
     def perform(job)
       url = URI.parse(job['url'])
@@ -31,6 +39,8 @@ module Fireworq
       req['content-type'] = 'application/json'
       req.body = job['payload'].to_json
       Net::HTTP.new(url.host, url.port).start {|http| http.request(req) }
+      # Ignore the response (especially an error response); it is not
+      # easy to control retry timing in Sidekiq.
     end
   end
 end
--- script/server.rb
+++ script/server.rb
@@ -11,12 +11,14 @@ opts = {
   :web_port          => 8080,
   :web_max_workers   => 10,
   :queue_max_workers => 5,
+  :queue_name        => 'fireworq',
 }
 
 opt = OptionParser.new
 opt.on('--web-port <port>') {|v| opts[:web_port] = v.to_i }
 opt.on('--web-max-workers <num>') {|v| opts[:web_max_workers] = v.to_i }
 opt.on('--queue-max-workers <num>') {|v| opts[:queue_max_workers] = v.to_i }
+opt.on('--queue-name <name>') {|v| opts[:queue_name] = v }
 opt.parse!(ARGV)
 
 FileUtils.mkdir_p(File.dirname(ENV_FILE))
@@ -27,6 +29,7 @@ RUBYLIB=.:#{ENV['RUBYLIB']}
 WEB_PORT=#{opts[:web_port]}
 WEB_MAX_WORKERS=#{opts[:web_max_workers]}
 QUEUE_MAX_WORKERS=#{opts[:queue_max_workers]}
+QUEUE_NAME=#{opts[:queue_name]}
 EOF
 end
 
